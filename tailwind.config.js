252c3734afbbc1c09414110a8d47f7ec7cb5ad74
Salahuddin33102 <dsalahuddin10@gmail.com>
2024-06-25 11:36:44 +0500

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+// 	content: [
+// 		"./pages/*/.{js,ts,jsx,tsx}",
+// 		"./components/*/.{js,ts,jsx,tsx}",
+// 	],
+// 	theme: {
+// 		extend: {
+// 			colors: {
+// 				primary: "#00AEEF",
+// 				primaryHover: "#000",
+// 				secondary: "#f8f8f8",
+// 			},
+// 			boxShadow: {
+// 				custom: '10px 10px 0px 0px rgba(0, 0, 0, 0.1)',
+// 			},
+// 			backgroundImage: {
+// 				"mbl-background": "url('/images/mblbackground.png')",
+// 			},
+// 		},
+// 		fontFamily: {
+// 			'noto': ['__Noto_Sans_7c0e9e'],
+// 		}
+// 	},
+// 	plugins: [],
+// };
